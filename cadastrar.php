930398494ecf0792748ce7c048170320535187f2
@Livia2710 <livia.sesi166@gmail.com>
2024-02-08 15:03:42 -0300

    ytr

--- cadastrar.php
+++ cadastrar.php
@@ -1,60 +1,57 @@
 <?php
 
 
-// Incluir a conexão com o banco de dados
+<?php
+
+// Incluir a conexao com o banco de dados
 include_once "conexao.php";
 
 // Receber os dados do formulário via método POST
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-//Validar o formulário
+// validar o formulario
 if (empty($dados['nome'])) {
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo nome!"];
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo nome!</div>"];
 } elseif (empty($dados['email'])) {
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo e-mail!"];
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo e-mail!</div>"];
 } elseif (empty($dados['logradouro'])) {
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo logradouro!"];
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo logradouro!</div>"];
 } elseif (empty($dados['numero'])) {
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo número!"];
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo número!</div>"];
 } else {
-    //Cadastrar no banco de dados na primeira tabela (tabela de usuarios)
+    // Cadastrar no banco de dados na primeira tabela (tabela de usuários)
     $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email)";
-    $card_usuario = $conn->prepare($query_usuario);
-    $card_usuario->bindParam(':nome', $dados['nome']);
-    $card_usuario->bindParam(':email', $dados['email']);
-    $card_usuario->execute();
+    $cad_usuario = $conn->prepare($query_usuario);
+    $cad_usuario->bindParam(':nome', $dados['nome']);
+    $cad_usuario->bindParam(':email', $dados['email']);
+    $cad_usuario->execute();
 
-    //Verificar se o usuario foi cadastro com sucesso
-    if ($card_usuario->rowCount()) {
-        //Recuperar o último ID inserido (ID do usuário recém-cadastrada)
+    // Verificar se o usuário foi cadastrado com sucesso
+    if ($cad_usuario->rowCount()) {
+        // Recuperar o último ID inserido (ID do usuário recém-cadastrado)
         $id_usuario = $conn->lastInsertId();
 
-        //Cadatrar no banco de dados na segunda tabela (tabela de endereço) associada ao usuário
-        $query_endereco = "INSERT INTO enderecos (logradouro, numero, usuario_id) VALUE (:logradouro, :numero, :usuario_id)";
-        $card_endereco = $conn->prepare($query_endereco);
-        $card_endereco->bindParam(':logradouro', $dados['logradouro']);
-        $card_endereco->bindParam(':numero', $dados['numero']);
-        $card_endereco->bindParam(':usuario_id', $usuario_id);
-        $card_endereco->execute();
+        // Cadastrar no banco de dados na segunda tabela (tabela de endereços) associada ao usuário
+        $query_endereco = "INSERT INTO enderecos (logradouro, numero, usuario_id) VALUES (:logradouro, :numero, :usuario_id)";
+        $cad_endereco = $conn->prepare($query_endereco);
+        $cad_endereco->bindParam(':logradouro', $dados['logradouro']);
+        $cad_endereco->bindParam(':numero', $dados['numero']);
+        $cad_endereco->bindParam(':usuario_id', $id_usuario);
+        $cad_endereco->execute();
 
-        //Verificar se o endereço foi cadatrado com sucesso
-        if ($card_endereco->rowCount()) {
-            //Se ambos, usuario e endereço, forem cadatrados com sucesso, retornar mensagem de sucesso
-            $retorna = ['status' => true, 'msg'=> "<div class='alert alert-sucess' role='alert'>Usuário cadastrado com sucesso!</div>"];
+        // Verificar se o endereço foi cadastrado com sucesso
+        if ($cad_endereco->rowCount()) {
+            // Se ambos, usuário e endereço, forem cadastrados com sucesso, retornar mensagem de sucesso
+            $retorna = ['status' => true, 'msg' => "<div class='alert alert-success' role='alert'>Usuário cadastrado com sucesso!</div>"];
         } else {
-            //Se o usuario for cadatrado com sucesso, mas o endereço não, retornar mensagem de erro
-            $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'> Erro: Usuário não cadastrado com sucesso!</div>"];
+            // Se o usuário foi cadastrado com sucesso, mas o endereço não, retornar mensagem de erro
+            $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Usuário não cadastrado com sucesso!</div>"];
         }
     } else {
-         //Se houver erro ao cadastrar o usuario, retornar mensagem de erro
-         $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'> Erro: Usuário não cadastrado com sucesso!</div>"];
+        // Se houver erro ao cadastrar o usuário, retornar mensagem de erro
+        $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Usuário não cadastrado com sucesso!</div>"];
     }
-
 }
 
 // Retornar o resultado em formato JSON
 echo json_encode($retorna);
-
-
-
-?>
