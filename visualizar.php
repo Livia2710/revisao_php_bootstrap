b194cc0827941ba31c9a82e41eff4d1570478cb0
@Livia2710 <livia.sesi166@gmail.com>
2024-02-08 11:28:58 -0300

    fd

--- visualizar.php
+++ visualizar.php
@@ -1,40 +1,36 @@
 <?php
 
-//Incluir a conexão com o banco de dados;
+// Incluir a conexão com o banco de dados
 include_once "conexao.php";
 
-
-// Receber o ID  via método GET
+// Receber o ID via método GET
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-// Verificar se o ID não está vazia
+// Verifica sw o ID não está vazio
 if (!empty($id)) {
-
-    // QUERY para selecionar informações do usuario e edereço associado
+    // Query para selecionar informações do usuário e enderço associado
     $query_usuario = "SELECT usr.id, usr.nome, usr.email,
             ende.logradouro, ende.numero
-            FROM usuarios AS usr 
+            FROM usuarios AS usr
             LEFT JOIN enderecos AS ende ON ende.usuario_id=usr.id
             WHERE usr.id=:id LIMIT 1";
-    $result_usuario = $conn->prepare($query_usuario);
+    $result_usuario = $conn-> prepare($query_usuario);
     $result_usuario->bindParam(':id', $id);
     $result_usuario->execute();
 
-    // Verificar se a consulta foi bem-sucedida
-    if (($result_usuario) and ($result_usuario->rowCount() != 0)) {
-        //Recupera os dados do usuario e endereço associado
+    // Verifica se a consulta doi bem sucedida e se h-a registros
+    if (($result_usuario) and ($result_usuario->rowCount() !=0)) {
+        // Recupera os dados do usuário e endereço associado
         $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
-        $retorna = ['statuts' => true, 'dados' => $row_usuario];
-       
-    } else {
-        //Se não houver registros , retorna uma mensagem de erro
-        $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
+        $retorna = ['status' => true, 'dados' => $row_usuario];
+    }else{
+        // Se não houver registros, retorna uma margem de erro
+        $retorna = ['status' => false, 'msg' => "<div class='alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
     }
 } else {
-    //Se não o ID estiver vazio , retorna uma mensagem de erro
-    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
+    // Se o ID estiver vazio, retorna uma mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
 }
 
-// Retornar o resultado em formato JSON
+// Retorna o resultado em formato JSON
 echo json_encode($retorna);
-?>
